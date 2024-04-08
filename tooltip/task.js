45d1e3c8c36544953e83c7d7bc55503b866c82db
Alexsander <sparco@nxt.ru>
2024-04-08 10:54:21 +0500

    tooltip dz correct

--- tooltip/task.js
+++ tooltip/task.js
@@ -1,18 +1,22 @@
 const hasTooltips = document.querySelectorAll('.has-tooltip')
 
 hasTooltips.forEach(function (hasTooltip) {
-    hasTooltip.addEventListener('click', function addDiv(event) {
+    hasTooltip.addEventListener('click', function (event) {
         event.preventDefault()
-        const height = hasTooltip.getBoundingClientRect().height
-        const left = hasTooltip.getBoundingClientRect().left
-        const top = hasTooltip.getBoundingClientRect().top
-        const divTooltip = document.createElement('div')
-        const title = hasTooltip.getAttribute('title')
-        divTooltip.className = 'tooltip'
-        divTooltip.innerHTML = title
-        hasTooltip.after(divTooltip)
-        divTooltip.classList.add('tooltip_active')
-        divTooltip.style.cssText = `left: ${left}px; top: ${top+height}px`
-    }, {once: true})
+        if(document.querySelector('.tooltip')) {
+            document.querySelector('.tooltip').remove()
+        }else{
+            const height = hasTooltip.getBoundingClientRect().height
+            const left = hasTooltip.getBoundingClientRect().left
+            const top = hasTooltip.getBoundingClientRect().top
+            const divTooltip = document.createElement('div')
+            const title = hasTooltip.getAttribute('title')
+            divTooltip.className = 'tooltip'
+            divTooltip.innerHTML = title
+            hasTooltip.after(divTooltip)
+            divTooltip.classList.toggle('tooltip_active')
+            divTooltip.style.cssText = `left: ${left}px; top: ${top+height}px`
+        }
+    })
 })
 
